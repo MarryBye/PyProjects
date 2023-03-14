044cdab418ba1800ea70e1a85012797daac2dbb2
MarryBye <viktorlukianov@outlook.com>
2023-03-14 13:50:26 +0200

    Update

---
+++ AnatolBot/main.py
@@ -0,0 +1,53 @@
+import asyncio
+
+import discord, os
+from discord.ext import commands
+from lib.funcs import *
+
+main_bot = Bot(command_prefix='#', intents=discord.Intents.all(), help_command=None)
+
+
+@main_bot.event
+async def on_ready():
+    await main_bot.check_data_folder()
+
+
+@main_bot.event
+async def on_message(msg):
+    level_up = await main_bot.change_member_stats(msg.author.id, msg.guild.id, 1, 0.5 * (len(msg.content) * 0.1))
+    if level_up:
+        await msg.add_reaction('🆙')
+
+    await main_bot.process_commands(msg)
+
+
+@main_bot.event
+async def on_raw_reaction_add(payload):
+    guild = main_bot.get_guild(payload.guild_id)
+    channel = guild.get_channel(payload.channel_id)
+    msg = await channel.fetch_message(payload.message_id)
+    member = guild.get_member(payload.user_id)
+
+    reaction_table = await main_bot.load_data(guild.id, main_bot.datatypes[2])
+
+    await main_bot.on_reaction_message(member, payload.emoji, guild, msg.id, give_type=0)
+
+
+@main_bot.event
+async def on_raw_reaction_remove(payload):
+    guild = main_bot.get_guild(payload.guild_id)
+    channel = guild.get_channel(payload.channel_id)
+    msg = await channel.fetch_message(payload.message_id)
+    member = guild.get_member(payload.user_id)
+
+    reaction_table = await main_bot.load_data(guild.id, main_bot.datatypes[2])
+
+    await main_bot.on_reaction_message(member, payload.emoji, guild, msg.id, give_type=1)
+
+
+async def main():
+    async with main_bot:
+        await main_bot.start("OTI3ODY4Nzc2MDM5NDU2Nzk4.GBjiJ_.DdO_XiDVvMJAWmQmy-BujOw1upWHGuuzRItuW4")
+
+
+asyncio.run(main())
